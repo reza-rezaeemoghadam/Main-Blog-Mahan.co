6bf26c97aa997df76e66f3dd3bff8d962cdab0c7
reza-rezaeemoghadam <rezarezaee678@gmail.com>
2024-11-23 00:10:54 +0330

    Implementing comment views
    Implementing comment and post  views, serailizer and fixing minor problems

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,8 +1,11 @@
 # Importing restframework modules
 from rest_framework.urls import path
 
+# Import views
+from blog.api.v1.views import CommentCreateAPIView
+
 app_name = 'user'
 
 urlpatterns = [
-
+    path("post/comment/", CommentCreateAPIView.as_view(), name="comment-insert"),
 ]
