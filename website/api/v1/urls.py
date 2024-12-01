db8a75c1409f862a5a9e3e0e105542202fc839f3
Reza Rezaee Moghadam <rezarezaeemoghadam@mahanpardaz.local>
2024-12-01 10:36:14 +0330

    [NOT TESTED] Implementing website views [NOT TESTED]
    Some minor bugs in serializer fixed

--- website/api/v1/urls.py
+++ website/api/v1/urls.py
@@ -2,10 +2,11 @@
 from rest_framework.urls import path
 
 # Importing custom views
+from website.api.v1.veiws import SectionRetrieveAPIView
 
 # Implementing website urls
 app_name = "website"
 
 urlpatterns = [
-    path()
+    path('website/<int:section_id>/', SectionRetrieveAPIView.as_view(), name="section_retrieve"),
 ]
--- website/api/v1/veiws.py
+++ website/api/v1/veiws.py
@@ -1,7 +1,15 @@
 # Importing rest framework modules
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
 
 # Importing custom Serializers
- 
+from website.api.v1.serializers import FullPortionSectionSerializers  
+
+# Importing custom models
+from website.models import Section
+
 # Implementing views
 class SectionRetrieveAPIView(RetrieveAPIView):
+    serializer_class = FullPortionSectionSerializers
+    permission_classes = [AllowAny]
+    model = Section
